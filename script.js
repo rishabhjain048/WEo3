22cb588dae90cc664fea79c224b840cdd2c0b7f9
Rishabhjain048 <92505685+rishabhjain048@users.noreply.github.com>
2024-03-03 22:26:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// document.getElementById("myBtn")
+// .addEventListener("click",function displayDate() {
+//     document.getElementById("demo").innerHTML=Date();
+// });
+
+// document.getElementById("myBtn").addEventListener("click",function(){
+//     alert("Are you 18+!");
+// });
+
+
+// var x= document.getElementById("myBtn");
+
+// x.addEventListener("click",myFunction);
+// x.addEventListener("click",otherOne);
+// x.addEventListener("click",displayDate);
+
+// function myFunction(){
+//     alert("hello Wrorld!");
+// }
+
+// function otherOne(){
+//     alert("welcome to the Rjain world!")
+// }
+// function displayDate(){
+//     document.getElementById("demo").innerHTML=Date();
+// }
+
+// var http=require('http');
+
+// http.createServer(function(req,res){
+//     res.writeHead(200,{'content-type':'text'})
+// })
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++rough^^^^work+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
